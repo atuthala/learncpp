ea20b577a5dbda2d26ba0a530ef7c9c6bae5a61c
atutsashaAsus <sasha2015to@gmail.com>
2024-12-16 20:04:10 +0300

    ch17_summary_Question3

--- ch17/ch17_summary/main.cpp
+++ ch17/ch17_summary/main.cpp
@@ -1,7 +1,6 @@
 // https://www.learncpp.com/cpp-tutorial/chapter-17-summary-and-quiz/
 
 #ifdef Question2
-#endif // Question2
 
 #include <array>
 #include <iostream>
@@ -160,6 +159,121 @@ int main()
 	std::cout << "\nThanks for shopping at Roscoe's potion emporium!\n";
 	return 0;
 }
+
+#endif // Question2
+
+
+
+#ifdef Question3
+#endif // Question3
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <iostream>
+#include "../../learncppGeneral/Random.h"
+
+
+// Because identifiers can't start with a number, we'll use a "rank_" prefix for these
+struct Card
+{
+	enum Rank
+	{
+		rank_ace,
+		rank_2,
+		rank_3,
+		rank_4,
+		rank_5,
+		rank_6,
+		rank_7,
+		rank_8,
+		rank_9,
+		rank_10,
+		rank_jack,
+		rank_queen,
+		rank_king,
+
+		max_ranks
+	};
+
+	// We'll also prefix these for consistency
+	enum Suit
+	{
+		suit_club,
+		suit_diamond,
+		suit_heart,
+		suit_spade,
+
+		max_suits
+	};
+
+	// These need to be static so they are only created once per program, not once per Card
+	static constexpr std::array allRanks{rank_ace, rank_2, rank_3, rank_4, rank_5, rank_6, rank_7, rank_8, rank_9, rank_10, rank_jack, rank_queen, rank_king};
+	static constexpr std::array allSuits{suit_club, suit_diamond, suit_heart, suit_spade};
+
+	Rank rank{};
+	Suit suit{};
+
+	friend std::ostream& operator<<(std::ostream& out, const Card &card)
+	{
+		static constexpr std::array ranks{'A', 'Z', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};
+		static constexpr std::array suits{'C', 'D', 'H', 'S'};
+
+		out << ranks[card.rank] << suits[card.suit];
+		return out;
+	}
+
+	int value() const
+	{
+		static constexpr std::array rankValues{11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+		return rankValues[rank];
+	}
+};
+
+class Deck
+{
+	private:
+		std::array<Card, 52> m_cards {};
+		std::size_t m_nextCardIndex{0};
+
+	public:
+		Deck()
+		{
+			std::size_t count {0};
+			for (auto suit: Card::allSuits)
+				for (auto rank: Card::allRanks)
+					m_cards[count++] = Card{rank, suit};
+		}
+
+		void shuffle()
+		{
+			std::shuffle(m_cards.begin(), m_cards.end(), Random::mt);
+			m_nextCardIndex = 0;
+		}
+
+		Card dealCard()
+		{
+			assert(m_nextCardIndex != 52 && "Deck::dealCard ran out of cards");
+			return m_cards[m_nextCardIndex++];
+		}
+};
+
+
+
+int main()
+{
+	Deck deck{};
+    std::cout << deck.dealCard() << ' ' << deck.dealCard() << ' ' << deck.dealCard() << '\n';
+
+    deck.shuffle();
+    std::cout << deck.dealCard() << ' ' << deck.dealCard() << ' ' << deck.dealCard() << '\n';
+
+	return 0;
+}
+
+
+
+
+
 
 
 
